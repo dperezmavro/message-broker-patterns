837060aae8911c919818ff5479e78dbe779e9e1c
dperezmavro <dio.pm@live.com>
2026-06-12 13:52:31 +0200

    added quorum fronted by a load balaner

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the RabbitMQ provider
 provider "rabbitmq" {
-  endpoint = "http://localhost:15673"
+  endpoint = "http://localhost:15672"
   username = "admin"
   password = "secretpassword123"
 }
